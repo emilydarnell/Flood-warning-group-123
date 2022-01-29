c0f1f07af1133294adb52f6ef67320eed4861b00
Emily Darnell <ed589@cam.ac.uk>
2022-01-29 19:32:47 +0000

    Test 1C

--- test_1C.py
+++ test_1C.py
@@ -1,13 +1,23 @@
 # IMPORT NECESSARY FUNCTIONS #
+from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
-#tests that only distances above r are included#
-def test_stations_within_radius():
+#tests that stations are put into alphabetical order#
+def test_stations_within_radius1():
+    stations = build_station_list()
+    city_centre_coord = (52.2053, 0.1218)
+    test = stations_within_radius(stations, city_centre_coord, 3)
+
+    assert test == ['Bin Brook', 'Cambridge Jesus Lock']
+
+test_stations_within_radius1()
+#tests that only distances below r are included#
+def test_stations_within_radius2():
     stations = build_station_list()
     city_centre_coord = (52.2053, 0.1218)
     test = stations_within_radius(stations, city_centre_coord, 0)
 
     assert test == []
 
-test_stations_within_radius()
+test_stations_within_radius2()
