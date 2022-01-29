fe5b4cd54b1923daa95a73918b17ca12d22ed2c8
Emily Darnell <ed589@cam.ac.uk>
2022-01-29 19:17:29 +0000

    Test 1C

---
+++ test_1C.py
@@ -0,0 +1,13 @@
+# IMPORT NECESSARY FUNCTIONS #
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+#tests that none of the distances or more than r#
+def test_stations_within_radius():
+    stations = build_station_list()
+    city_centre_coord = (52.2053, 0.1218)
+    test = stations_within_radius(stations, city_centre_coord, 0)
+
+    assert test == []
+
+test_stations_within_radius()
