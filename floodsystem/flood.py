ed34d882478b5f8fe0ff199a815819d2186f4d33
Alex Beeston <alex@beestons.net>
2022-02-17 12:11:49 +0000

    Task 2C update

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -17,3 +17,20 @@ def stations_level_over_threshold(stations, tol):
     #sort the dictionary into order by magnitude of relative water level#
     sorted_over_threshold_dict = sorted_by_key(over_threshold_dict, 1, reverse=True)
     return sorted_over_threshold_dict
+
+
+def stations_highest_rel_level(stations, N):
+    #create dictionary to store the tuples#
+    highest_level_dict = []
+
+    #check each station one by one#
+    for station in stations:
+        #check that there is no inconsistent data#
+        if station.relative_water_level() is not None:
+                station_tol_tuple = (station.name, MonitoringStation.relative_water_level(station))
+                highest_level_dict.append(station_tol_tuple)
+    #sort the dictionary into order by magnitude of relative water level#
+    sorted_highest_level_dict = sorted_by_key(highest_level_dict, 1, reverse=True)
+    #only return the first N entries#
+    return sorted_highest_level_dict[:N]
+
