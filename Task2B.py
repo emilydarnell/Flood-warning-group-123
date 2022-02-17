81fc7baa3143311b41d52e465884348b7f6289aa
Alex Beeston <alex@beestons.net>
2022-02-17 11:31:48 +0000

    Task 2B docstring

--- Task2B.py
+++ Task2B.py
@@ -1,13 +1,12 @@
 from floodsystem.flood import stations_level_over_threshold
-from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
 from floodsystem.stationdata import update_water_levels
-import floodsystem.station as fs
 
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
 
+    print(stations_level_over_threshold(stations, 0.8))
 
-
-stations = build_station_list()
-update_water_levels(stations)
-
-print(stations_level_over_threshold(stations, 0.8))
+if __name__ == "__main__":
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -3,12 +3,17 @@ from .utils import sorted_by_key
 
 
 def stations_level_over_threshold(stations, tol):
-    
+    #create new dictionary to store the tuples#
     over_threshold_dict = []
+
+    #check each station one by one#
     for station in stations:
+        #check that there is no inconsistent data#
         if station.relative_water_level() is not None:
+            #if the relative level is greater than the chosen tolerance, then add it to the dictionary#
             if station.relative_water_level() > tol:
                 station_tol_tuple = (station.name, MonitoringStation.relative_water_level(station))
                 over_threshold_dict.append(station_tol_tuple)
+    #sort the dictionary into order by magnitude of relative water level#
     sorted_over_threshold_dict = sorted_by_key(over_threshold_dict, 1, reverse=True)
     return sorted_over_threshold_dict
