c0d2e48b15752ccf280bf58cbded2e67afd00c35
Emily Darnell <ed589@cam.ac.uk>
2022-02-26 13:40:58 +0000

    Task 2E

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -18,7 +18,10 @@ def plot_water_levels(station, dates, levels):
     plt.title(station.name)
 
 # plot the typical high/low ranges on the same graph
-
+    low_value = station.typical_range[0]
+    high_value = station.typical_range[1]
+    plt.axhline(low_value, 0, 1, color="red")
+    plt.axhline(high_value, 0, 1, color = "red")
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
     plt.show()
